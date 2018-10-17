8650a1125979e5f632b31570b1c5b8f644fea22d
Kettenbach, Thomas <thomas.kettenbach@oath.com>
2018-10-17 11:09:51 +0200

    throw uncheckedioexcpetion instead

--- ListSymlinks.java
+++ ListSymlinks.java
@@ -1,15 +1,20 @@
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public final class ListSymlinks {
-    public static void list(final String directory) throws IOException {
-        Files.walk(Paths.get(directory)).filter(Files::isSymbolicLink)
-            .forEach(System.out::println);
+    public static void list(final String directory) {
+        try {
+            Files.walk(Paths.get(directory)).filter(Files::isSymbolicLink)
+                .forEach(System.out::println);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
     }
 
-    public static final void main(String...args) throws IOException {
+    public static final void main(String...args) {
         for (String a: args) {
             list(a);
         }
